0bccb65ab110b795e9ff0509f9f4d40f348c13ad
HF97 <hugoanf97@gmail.com>
2021-07-23 11:52:37 +0100

    Knights complee.

--- 1_Knowledge/knights/puzzle.py
+++ 1_Knowledge/knights/puzzle.py
@@ -12,21 +12,36 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Or(AKnight, AKnave),                            # either is a knigh or a knave
+    # Not(And(AKnight, AKnave)),                      # can't be both at same time
+    Biconditional(AKnight, Not(AKnave)),            # if a is knight can't be knave and if is a knave is not a knight
+    
+    Implication(AKnight, And(AKnight, AKnave)),     # if is a knight the sentece is true
+    Implication(AKnave, Not(And(AKnight, AKnave)))  # if is a knva the sentence is false
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Biconditional(AKnight, Not(AKnave)),            # if a is knight can't be knave and if is a knave is not a knight
+    Biconditional(BKnight, Not(BKnave)),            # if a is knight can't be knave and if is a knave is not a knight
+    
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Biconditional(AKnight, Not(AKnave)),            # if is knight can't be knave and if is knave is not a knight
+    Biconditional(BKnight, Not(BKnave)),            # if is knight can't be knave and if is knave is not a knight
+    
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
 )
 
 # Puzzle 3
@@ -35,7 +50,44 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    Biconditional(AKnight, Not(AKnave)),            # if is knight can't be knave and if is knave is not a knight
+    Biconditional(BKnight, Not(BKnave)),            # if is knight can't be knave and if is knave is not a knight
+    Biconditional(CKnight, Not(CKnave)),            # if is knight can't be knave and if is knave is not a knight
+    
+    # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Or(
+        And(Implication(AKnight, AKnight), Implication(AKnave, Not(AKnight))),
+                                                    # if a is knight
+        And(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave)))
+                                                    # if a is knave
+    ),
+    Not(                                            # cant be both a knight and a knave
+        And(
+            And(
+                Implication(AKnight, AKnight),
+                Implication(AKnave, Not(AKnight))
+            ),
+        
+            And(
+                Implication(AKnight, AKnave),
+                Implication(AKnave, Not(AKnave))
+            )
+        )
+    ),
+
+    # B says "A said 'I am a knave'."
+    Implication(BKnight, And(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave)))),
+                                                    # b knight then if a knight then knave, if a knave then a not knave
+    Implication(BKnave, Not(And(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave))))),
+                                                    # b knight then if a knight then knave and if a knave then a not knave is false
+
+    # B says "C is a knave."
+    Implication(BKnight, CKnave),                   # b knight then c knave
+    Implication(BKnave, Not(CKnave)),               # b knave then c not knave
+
+    # C says "A is a knight."
+    Implication(CKnight, AKnight),                  # c knight then a is knight
+    Implication(CKnave, Not(AKnight)),              # c is knave then a is not knight
 )
 
 
